0acdbb1c5d6830a71c9ab89940b4a2977af5a1cf
FaroukZeino <33427443+FaroukZeino@users.noreply.github.com>
2021-12-16 00:45:17 +0300

    fix contract

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -4,7 +4,6 @@ pragma solidity >=0.4.22 <0.9.0;
 contract TodoList {
     mapping(uint256 => Task) public tasks;
     uint256 public taskCount = 0;
-    address owner = msg.sender;
 
     event TaskCreated(uint256 id, string content, bool finished);
     event TaskFinished(uint256 id, bool finished);
@@ -20,14 +19,12 @@ contract TodoList {
     }
 
     function createTask(string memory _content) public {
-        require(msg.sender == owner, "Not the owner!");
         taskCount = taskCount + 1;
         tasks[taskCount] = Task(taskCount, _content, false);
         emit TaskCreated(taskCount, _content, false);
     }
 
     function toggleFinished(uint256 _id) public {
-        require(msg.sender == owner, "Not the owner!");
         Task memory _task = tasks[_id];
         _task.finished = !_task.finished;
         tasks[_id] = _task;
